ae3bd73b346761c84ca4f22cf68a27a16de3f107
Jack MB <jackmb@Jacks-iMac.local>
2020-09-23 17:57:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+
+
+
+(function() {
+  // Init
+  var container = document.getElementById("aerial-anim-container"),
+  inner = document.getElementById("aerial-anim");
+
+  // Mouse
+  var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function(event) {
+      var e = event || window.event;
+      this.x = e.clientX - this._x;
+      this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function(e) {
+      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function() {
+      return "(" + this.x + ", " + this.y + ")";
+    }
+  };
+
+  // Track the mouse position relative to the center of the container.
+  mouse.setOrigin(container);
+
+  //-----------------------------------------
+
+  var counter = 0;
+  var updateRate = 10;
+  var isTimeToUpdate = function() {
+    return counter++ % updateRate === 0;
+  };
+
+  //-----------------------------------------
+
+  var onMouseEnterHandler = function(event) {
+    update(event);
+  };
+
+  var onMouseMoveHandler = function(event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  //-----------------------------------------
+
+  var update = function(event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+      (mouse.y / 20).toFixed(2),
+      (mouse.x / 10).toFixed(2)
+    );
+  };
+
+  var updateTransformStyle = function(x, y) {
+    x = 50 - x;
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTransform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+  };
+
+  //-----------------------------------------
+
+  document.onmousemove = onMouseMoveHandler;
+})();
